095b92be6d2f8b9a3a0de3883db2c131f4019d2d
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-05 20:10:27 -0800

    added Error page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.scss';
-
+import Error404  from './components/Error404'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-   
+   <Error404 />
       </header>
     </div>
   );
---
+++ src/components/Error404.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class Error404 extends React.Component {
+    render() {
+        return (
+            <div className='no-page'>Nothing Here
+                
+            </div>
+        );
+    }
+}
