441cc6f46d6a1b7400c889a81a5828539e5a4d1c
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-05 20:23:17 -0800

    added AddTrip component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.scss';
 import Error404  from './components/Error404'
 import Nav from './components/Nav'
+import { AddTrip } from './components/trips/AddTrip';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-   <Nav />
+   <AddTrip />
       </header>
     </div>
   );
